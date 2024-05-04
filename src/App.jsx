dea834e343c221b50e1dfe45e2b3367b5c1a5684
jmella22 <ing.jose.mella@gmail.com>
2024-05-04 01:27:33 -0400

    resolucion del problema

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import s from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [estado, setEstado] = useState("pendiente");
+  const [result, setResult] = useState(0);
+  const [input, setInput] = useState({
+    anchoTecho: 0,
+    largoTecho: 0,
+    anchoPanel: 0,
+    largoPanel: 0,
+  });
+
+  const calculate = (at, lt, ap, lp) => {
+    const minT = at > lt ? lt : at;
+    if (at == 0 || lt == 0 || ap == 0 || lp == 0) {
+      return 0;
+    }
+    if (ap > minT && lp > minT) {
+      return 0;
+    } else {
+      return Math.floor((at * lt) / (ap * lp));
+    }
+  };
+
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSumit = (e) => {
+    e.preventDefault();
+
+    setResult(
+      calculate(
+        input.anchoTecho,
+        input.largoTecho,
+        input.anchoPanel,
+        input.largoPanel
+      )
+    );
+    setEstado("resuelto");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <main>
+        <h1>Prueba tecnica Ruuf - paneles Solares</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Programa que consiste en calcular la cantidad maxima de paneles
+          solares que caben en una superfice (techo).
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <form className={s.form} onSubmit={(e) => handleSumit(e)}>
+          <div className={s.space}>
+            <p>Dimensiones del techo en metros</p>
+            <input
+              type="number"
+              min={1}
+              id="largoTecho"
+              placeholder="Largo del techo"
+              className={s.input}
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              min={1}
+              id="anchoTecho"
+              placeholder="Ancho del techo"
+              className={s.input}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={s.space}>
+            <p>Dimensiones del Panel Solar en metros</p>
+            <input
+              type="number"
+              min={1}
+              id="largoPanel"
+              placeholder="Largo del Panel"
+              className={s.input}
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              min={1}
+              id="anchoPanel"
+              placeholder="Ancho del Panel"
+              className={s.input}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Calcular</button>
+        </form>
+        {estado == "resuelto" && (
+          <div>
+            <p className={s.result}>
+              La cantidad de paneles que caben en el techo es de{" "}
+              <strong className={s.decorate}>{result}</strong>
+            </p>
+          </div>
+        )}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
